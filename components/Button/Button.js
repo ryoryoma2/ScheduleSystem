aec235859511ad2b4dbc0d81a432bbac3ab934c7
Kousuke-Mitsugi <75293501+Kousuke-Mitsugi@users.noreply.github.com>
2021-01-12 00:22:09 +0900

    Add files via upload

--- components/Button/Button.js
+++ components/Button/Button.js
@@ -3,18 +3,17 @@ import { Link } from 'react-router-dom';
 import './Button.css';
 
 const Button = (props) => {
-  return (
-    <div class="flex">
-      <button
-        onClick={props.onClick}
-        id="Button"
-        className={props.className}
-        class={props.className}
-      >     <Link to={props.linkname} >{props.buttonname}</Link>
-      </button>
-    </div>
+    return (
+        <div class="flex">
+            <button
+                onClick={props.onClick}
+                id="Button"
+                className={props.className}
+            >     <Link to={props.linkname} >{props.buttonname}</Link>
+            </button>
+        </div>
 
-  );
+    );
 }
 
 export default Button;
