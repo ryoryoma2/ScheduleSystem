838fa0562923af6528802022dc71480b14e465cc
narumia0327 <73158256+narumia0327@users.noreply.github.com>
2021-01-14 20:20:09 +0900

    更新

--- src/HomeworkEdit_v2.js
+++ src/HomeworkEdit_v2.js
@@ -1,16 +1,12 @@
 // インポート
 import React, { Component } from 'react';
-import './App.css';
-import homeworkSample from './images/homeworkSample.jpeg';
+import './Homeworkedit.css';
+// import Button from './components/Button/Button';
 
-const style = {
-    width: '100%',
-    height: '30rem',
-    backgroundImage: `url(${homeworkSample})`
-};
+
 
 // ファイルの実質上の中身
-class confirmHomework extends Component {
+class Homeworkedit extends Component {
 
     // イベントハンドラー
     onClickHandler = () => {
@@ -20,7 +16,7 @@ class confirmHomework extends Component {
 
     render() {
         return (
-            <div className="confirmHomework">
+            <div className="Homeworkedit">
                 <header className="App-header">
                     <p>
                         <input type="search" name="studentIDSearch" placeholder="生徒IDを入力"></input>
@@ -30,19 +26,24 @@ class confirmHomework extends Component {
                         <tr>
                             <th bgcolor="#FFFFFF"><font color="#000000">科目</font></th>
                             <th bgcolor="#FFFFFF"><font color="#000000">範囲</font></th>
-                            <th bgcolor="#FFFFFF"><font color="#000000">提出済</font></th>
                         </tr>
                         <tr>
                             <td bgcolor="#FFFFFF" align="center" nowrap width="0%"><font color="#000000">英語</font></td>
-                            <td bgcolor="#FFFFFF" align="center" nowrap width="0%"><font color="#000000">p.11-15</font></td>
                             <td bgcolor="#FFFFFF" align="center">
-                                <img style={style} />
+                                <p>
+                                    <input type="text" name="宿題"></input>
+                                    <input type="submit" value="設定する"></input>
+                                </p>
                             </td>
                         </tr>
                         <tr>
                             <td bgcolor="#FFFFFF" align="center" nowrap><font color="#000000">数学</font></td>
-                            <td bgcolor="#FFFFFF" align="center" nowrap width="0%"><font color="#000000">p.11-15</font></td>
-                            <td bgcolor="#FFFFFF" align="center"><font color="#000000">未提出</font></td>
+                            <td bgcolor="#FFFFFF" align="center">
+                                <p>
+                                    <input type="text" name="宿題"></input>
+                                    <input type="submit" value="設定する"></input>
+                                </p>
+                            </td>
                         </tr>
                     </table>
                     <br></br>
@@ -56,4 +57,4 @@ class confirmHomework extends Component {
 }
 
 // エクスポート
-export default confirmHomework;
+export default Homeworkedit;
