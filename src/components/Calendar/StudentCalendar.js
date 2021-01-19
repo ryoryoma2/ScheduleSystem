453ddcbc0ecdea77653e02878164ccf7e8791e30
narumia0327 <73158256+narumia0327@users.noreply.github.com>
2021-01-19 13:37:44 +0900

    更新

--- src/components/Calendar/StudentCalendar.js
+++ src/components/Calendar/StudentCalendar.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import Calendar from 'react-calendar';
 
+//redux関係のインポート
+import { connect } from 'react-redux'
 import { useSelector } from "react-redux";
-
-const sutudentAttendance = [
-    {
-
-    }
-];
+import { useDispatch } from "react-redux";
 
 class Schedule_Calendar extends React.Component {
     constructor(props) {
@@ -16,18 +13,31 @@ class Schedule_Calendar extends React.Component {
             date: new Date(),
             //月のデータ
             month_days: {
-            }
+            },
+            studentAttendance: [
+                {
+                    date: '',
+                    is_attendance: false,
+                }
+            ]
         };
         this.getTileClass = this.getTileClass.bind(this);
         this.getTileContent = this.getTileContent.bind(this);
         this.handleChange = this.handleChange.bind(this);
     }
 
-    // useSelector(state => state.studentAttendance.date);
 
-    //チェック状態を確認したい
-    handleChange(e){
-        return e.target.checked;
+
+    //チェック状態を確認
+    handleChange(e) {
+        if (this.state.studentAttendance.includes(e.target.value)) {
+            // チェックが外れたので要素を消す
+            this.state.studentAttendance.splice(e.target.value, 1);
+        } else {
+            //　チェックがついたので日付をセットし,is_attendanceをtrueにする
+            this.setState({ ...this.state.studentAttendance, date: e.target.value });
+            this.setState({ ...this.state.studentAttendance.date[e.target.value], is_attendance: true });
+        }
     }
 
     // state の日付と同じ表記に変換
@@ -57,16 +67,16 @@ class Schedule_Calendar extends React.Component {
             <p>
                 <br />
                 {(this.state.month_days[day] && this.state.month_days[day].text) ?
-                    this.state.month_days[day].text :
-                    <div>
-                        <input
-                            type="checkbox"
-                            value={day}
-                            onChange={this.handleChange}
-                            //checked={}
-                        />
-                    </div>
+                    this.state.month_days[day].text : ''
                 }
+                <div>
+                    <input
+                        type="checkbox"
+                        value={day}
+                        onChange={this.handleChange}
+                        checked={this.state.studentAttendance.attendance.includes(day)}
+                    />
+                </div>
             </p>
         );
     }
@@ -83,4 +93,15 @@ class Schedule_Calendar extends React.Component {
     }
 }
 
-export default Schedule_Calendar;
+//export default Schedule_Calendar;
+
+function mapStateToProps(state) {
+    return state;
+}
+
+function mapDispatchToProps() {
+    const dispatch = useDispatch();
+    return { dispatch1: () => { dispatch(()); } };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Schedule_Calendar)
